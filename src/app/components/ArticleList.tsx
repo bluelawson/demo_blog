3ab073eefe2df25af042d17964bf03ad839037aa
“Satoshi <plus.cron@gmail.com>
2024-07-12 01:19:13 +0900

    初期コミット

---
+++ src/app/components/ArticleList.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { Article } from "@/types";
+import ArticleCard from "./ArticleCard";
+
+type ArticleListProps = {
+  articles: Article[];
+};
+
+const ArticleList = ({ articles }: ArticleListProps) => {
+
+  return (
+    <div>
+      {articles.map((article) => (
+        <ArticleCard article={article} key={article.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ArticleList;
