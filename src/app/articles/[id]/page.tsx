893f2add9a2721c0ca6061d19923de91ccfc987d
“Satoshi <plus.cron@gmail.com>
2024-07-19 02:04:22 +0900

    試しに画像をなくしてみる

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -17,12 +17,6 @@ const Article = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="max-w-3xl mx-auto p-5">
-      <Image
-        src={`/sample/articleImage.jpg`}
-        alt="no image"
-        width={128}
-        height={30}
-      />
       <h1 className="text-4xl text-center mb-10 mt-10">
         {detailArticle.title}
       </h1>
