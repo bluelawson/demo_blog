85e71289c46dbb361c5a60b0623a9d7fbacdc753
“Satoshi <plus.cron@gmail.com>
2024-08-08 21:54:54 +0900

    やっぱり画像は入れる

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -17,6 +17,12 @@ const Article = async ({ params }: { params: { id: string } }) => {
 
   return (
     <div className="max-w-3xl mx-auto p-5">
+      <Image
+        src={`/sample/articleImage.jpg`}
+        alt="no image"
+        width={1280}
+        height={300}
+      />
       <h1 className="text-4xl text-center mb-10 mt-10">
         {detailArticle.title}
       </h1>
