193033129253c25b259fa5992875e79d07eb5c70
“Satoshi <plus.cron@gmail.com>
2024-08-24 16:05:14 +0900

    ヘッダにあるボタンのレイアウトを修正

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -4,14 +4,17 @@ import React, { useEffect, useState } from "react";
 import { User } from "@supabase/supabase-js";
 import { supabase } from "@/utils/supabaseClient";
 import { usePathname, useRouter } from "next/navigation";
+import HeaderLoading from "./headerLoading";
 
 const Header = () => {
   const router = useRouter();
   const pathname = usePathname();
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     getUserData();
+    setLoading(false);
   }, [pathname]);
 
   const handleLogout = async () => {
@@ -40,22 +43,23 @@ const Header = () => {
         <Link href="/">Demo Blog</Link>
       </h1>
       <div>
-        <nav className="text-sm font-medium">
-          <Link href="/" className="bg-white-300 px-3 py-3 rounded-md">
+        {loading && <HeaderLoading />}
+        <nav className="text-sm" hidden={loading}>
+          <Link href="/" className="text-xs bg-white-300 px-3 py-3 rounded-md">
             {user ? `こんにちは、${user?.user_metadata.user_name} さん` : null}
           </Link>
           {!user ? (
             <>
               <Link
                 href="/login"
-                className="bg-orange-400 font-bold px-3 mx-3 py-3 rounded-lg"
+                className="text-xs px-2 mx-1 py-2 rounded-lg hover:bg-sky-700"
               >
                 <span className="i-tabler-key mr-3 relative top-[1px] scale-150"></span>
                 ログイン
               </Link>
               <Link
                 href="/signUp"
-                className="bg-orange-400 font-bold px-3 mx-3 py-3 rounded-lg"
+                className="text-xs px-2 mx-1 py-2 rounded-lg hover:bg-sky-700"
               >
                 <span className="i-tabler-user-plus mr-3 relative top-[1px] scale-150"></span>
                 新規登録
@@ -66,23 +70,23 @@ const Header = () => {
             <>
               <Link
                 href="/myPage"
-                className="bg-orange-400 font-bold px-3 mx-3 py-3 rounded-lg"
+                className="text-xs px-2 mx-1 py-2 rounded-lg hover:bg-sky-700"
               >
-                <span className="i-tabler-settings mr-3 relative top-[1px] scale-150"></span>
+                <span className="i-tabler-settings mr-2 relative top-[1px] scale-150"></span>
                 アカウント設定
               </Link>
               <button
                 onClick={handleLogout}
-                className="bg-orange-400 font-bold px-3 mx-3 py-3 rounded-lg"
+                className="text-xs px-2 mx-1 py-2 rounded-lg hover:bg-sky-700"
               >
-                <span className="i-tabler-lock mr-3 relative top-[1px] scale-150"></span>
+                <span className="i-tabler-lock mr-2 relative top-[1px] scale-150"></span>
                 ログアウト
               </button>
               <Link
                 href="/articles/new"
-                className="bg-orange-400 font-bold px-3 mx-3 py-3 rounded-lg"
+                className="bg-sky-500 font-bold px-3 mx-3 py-2 rounded-lg"
               >
-                <span className="i-tabler-edit mr-3 relative top-[1px] scale-150"></span>
+                <span className="i-tabler-edit mr-2 relative top-[1px] scale-150"></span>
                 記事投稿
               </Link>
             </>
