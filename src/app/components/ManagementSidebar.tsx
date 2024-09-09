b6afe1749c4825da676496bc29b576e0b2a165c9
“Satoshi <plus.cron@gmail.com>
2024-09-09 09:18:35 +0900

    サイドバーのデザインを改良 サイドバーの要素をクリックするとハイライトされるようにした

--- src/app/components/ManagementSidebar.tsx
+++ src/app/components/ManagementSidebar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 
 type Menu = {
@@ -7,24 +8,30 @@ type Menu = {
   link: string;
 };
 
-export default async function ManagementSidebar() {
+const ManagementSidebar = () => {
+  const [selectedMenuId, setSelectedMenuId] = useState<number>(2);
   const sidebarMenus: Menu[] = [
     { id: 1, name: "ホーム", link: "/management" },
     { id: 2, name: "記事一覧", link: "/management/articles" },
     { id: 3, name: "アカウント設定", link: "/management/myPage" },
     { id: 4, name: "アクセス解析", link: "#" },
   ];
+
+  const handleMenuSelect = async (menuId: number) => {
+    setSelectedMenuId(menuId);
+  };
+
   return (
     <div className="h-[80%] bg-slate-600">
       <nav className="text-base py-5 my-4">
         {sidebarMenus.map((sidebarMenu) => (
-          <div
-            className="ml-1 mr-3 border-b border-white "
-            key={sidebarMenu.id}
-          >
+          <div className="ml-1 mr-3 " key={sidebarMenu.id}>
             <Link
               href={sidebarMenu.link}
-              className="block pb-1 pt-6 px-3 hover:text-gray-300"
+              className={`block pb-1 pt-6 px-3 ${
+                selectedMenuId === sidebarMenu.id ? "bg-slate-500" : ""
+              }`}
+              onClick={() => handleMenuSelect(sidebarMenu.id)}
             >
               {sidebarMenu.name}
             </Link>
@@ -33,4 +40,6 @@ export default async function ManagementSidebar() {
       </nav>
     </div>
   );
-}
+};
+
+export default ManagementSidebar;
