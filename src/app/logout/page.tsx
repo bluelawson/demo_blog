370191477d84ddeeb4459a661742efedbaa498a8
bluelawson <plus.cron@gmail.com>
2024-09-14 22:58:42 +0900

    ログアウト画面が右にずれていたので修正

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 const Logout = () => {
   return (
     <>
-      <div className="h-[50vh] w-screen flex items-center justify-center">
+      <div className="h-[50vh] w-full flex items-center justify-center">
         <div className="text-center">
           <h1>ログアウトしました</h1>
-          <Link href="/" className="hover:bg-sky-700 underline">
+          <Link href="/" className="underline hover:bg-sky-700">
             ホームへ戻る
           </Link>
         </div>
