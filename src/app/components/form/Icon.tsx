259540a00b4e2d8b2146c4f3a030020b3afa4beb
bluelawson <plus.cron@gmail.com>
2024-09-16 10:41:42 +0900

    useEffect外してみる

--- src/app/components/form/Icon.tsx
+++ src/app/components/form/Icon.tsx
@@ -1,13 +1,10 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
 
 type IconProps = {
   iconName: string;
 };
 
 export default function Icon({ iconName }: IconProps) {
-  useEffect(() => {}, []);
-
   return (
     <span className={`i-tabler-${iconName} mr-2 relative top-[2px] `}></span>
   );
