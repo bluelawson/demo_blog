1382571927ef52f65cffcc78dbe31803718f6221
bluelawson <plus.cron@gmail.com>
2024-09-16 10:29:31 +0900

    iconが取れないので初期表示時に再レンダリングしてみる

--- src/app/components/form/Icon.tsx
+++ src/app/components/form/Icon.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 
 type IconProps = {
   iconName: string;
 };
 
 export default function Icon({ iconName }: IconProps) {
+  useEffect(() => {}, []);
+
   return (
     <span className={`i-tabler-${iconName} mr-2 relative top-[2px] `}></span>
   );
