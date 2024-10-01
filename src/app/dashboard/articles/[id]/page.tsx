3ff3f156f1adb34d3890a4df4bc71f5c21074fc9
bluelawson <plus.cron@gmail.com>
2024-10-02 03:07:53 +0900

    changes: 記事更新ページにエラーハンドリングを追加

--- src/app/dashboard/articles/[id]/page.tsx
+++ src/app/dashboard/articles/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { notFound, useRouter } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import {
   ButtonFrame,
@@ -15,6 +15,7 @@ import { useMessage } from '@/context/MessageContext';
 import { API_URL } from '@/utils/constants';
 
 const EditArticle = ({ params }: { params: { id: string } }) => {
+  const router = useRouter();
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [imageUrl, setImageUrl] = useState('');
@@ -22,52 +23,44 @@ const EditArticle = ({ params }: { params: { id: string } }) => {
   const [loading, setLoading] = useState(true);
   const { showErrorMessage, showSnackbarMessage } = useMessage();
 
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
+  const fetchArticle = useCallback(async () => {
+    setLoading(true);
     try {
-      const res = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
+      const response = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
         next: {
           revalidate: 10,
         },
       });
 
-      if (res.ok) {
-        const detailArticle = await res.json();
-        if (!detailArticle) {
-          notFound();
-        }
-        setTitle(detailArticle.title);
-        setContent(detailArticle.content);
-        setImageUrl(detailArticle.imageUrl);
-      } else {
-        console.error('Failed to fetch articles');
+      if (!response.ok) {
+        throw new Error(`Status Code is ${response.status}`);
       }
+
+      const detailArticle = await response.json();
+      if (!detailArticle) {
+        notFound();
+      }
+      setTitle(detailArticle.title);
+      setContent(detailArticle.content);
+      setImageUrl(detailArticle.imageUrl);
     } catch (error) {
-      console.error('Error fetching articles:', error);
+      console.error(error);
+      showErrorMessage('記事情報の取得に失敗しました');
     } finally {
       setLoading(false);
     }
-  };
+  }, [params.id, showErrorMessage]);
 
-  const router = useRouter();
-
-  const handleDelete = async () => {
-    const response = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
-      method: 'DELETE',
-    });
-    if (response.ok) {
-      showSnackbarMessage('削除が完了しました！');
-      router.push(`/dashboard/articles`);
-    } else {
-      showErrorMessage('削除に失敗しました');
-    }
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchArticle();
+    };
+    fetchData();
+  }, [fetchArticle]);
 
   const handleUpdate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
     let imageUrl = null;
     // 画像をSupabase storageにアップロードする
     if (uploadFile) {
@@ -75,41 +68,71 @@ const EditArticle = ({ params }: { params: { id: string } }) => {
       formData.append('file', uploadFile);
       formData.append('fileName', uploadFile.name);
 
-      const response = await fetch(`${API_URL}/api/blog/upload`, {
-        method: 'POST',
-        body: formData,
-      });
+      try {
+        const response = await fetch(`${API_URL}/api/blog/upload`, {
+          method: 'POST',
+          body: formData,
+        });
 
-      if (!response.ok) {
-        const error = await response.json();
-        console.error('Image upload error:', error.error);
+        if (!response.ok) {
+          throw new Error(`Status Code is ${response.status}`);
+        }
+
+        const { path } = await response.json();
+        imageUrl = path;
+      } catch (error) {
+        console.error(error);
+        showErrorMessage('画像のアップロードに失敗しました');
         setLoading(false);
         return;
       }
-
-      const { path } = await response.json();
-      imageUrl = path;
     }
 
     const id = params.id;
-    const response = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id,
-        title,
-        content,
-        imageUrl: imageUrl,
-      }),
-    });
-
-    if (response.ok) {
+    try {
+      const response = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          id,
+          title,
+          content,
+          imageUrl: imageUrl,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`Status Code is ${response.status}`);
+      }
       showSnackbarMessage('更新が完了しました！');
       router.push(`/dashboard/articles`);
-    } else {
+    } catch (error) {
+      console.error(error);
       showErrorMessage('更新に失敗しました');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${API_URL}/api/blog/posts/${params.id}`, {
+        method: 'DELETE',
+      });
+      if (response.ok) {
+        showSnackbarMessage('削除が完了しました！');
+        router.push(`/dashboard/articles`);
+      } else {
+        showErrorMessage('削除に失敗しました');
+      }
+    } catch (error) {
+      console.error(error);
+      showErrorMessage('削除に失敗しました');
+      return;
+    } finally {
+      setLoading(false);
     }
   };
 
